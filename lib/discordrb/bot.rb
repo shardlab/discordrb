b331e1082dd5321f746d34b14a956b8e2346b4bd
Matt Carey <matthew.b.carey@gmail.com>
2020-12-01 23:32:44 -0500

    fixup: transform message_reference from message_id to API object

--- lib/discordrb/bot.rb
+++ lib/discordrb/bot.rb
@@ -370,8 +370,9 @@ module Discordrb
       channel = channel.resolve_id
       debug("Sending message to #{channel} with content '#{content}'")
       allowed_mentions = { parse: [] } if allowed_mentions == false
+      message_reference = { message_id: message_reference.id } if message_reference
 
-      response = API::Channel.create_message(token, channel, content, tts, embed&.to_hash, nil, attachments, allowed_mentions&.to_hash, message_reference&.id)
+      response = API::Channel.create_message(token, channel, content, tts, embed&.to_hash, nil, attachments, allowed_mentions&.to_hash, message_reference)
       Message.new(JSON.parse(response), self)
     end
 
