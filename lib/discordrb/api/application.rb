aa24c6e103dc9765a7f28913e224d38a259e9d0c
Matthew Carey <matthew.b.carey@gmail.com>
2021-05-02 16:08:14 -0400

    Move interactions to their own namespace

--- lib/discordrb/api/application.rb
+++ lib/discordrb/api/application.rb
@@ -8,10 +8,22 @@ module Discordrb::API::Application
   # https://discord.com/developers/docs/interactions/slash-commands#get-global-application-commands
   def get_global_commands(token, application_id)
     Discordrb::API.request(
-      :applications_aid,
+      :applications_aid_commands,
       nil,
       :get,
       "#{Discordrb::API.api_base}/applications/#{application_id}/commands",
+      Authorization: token
+    )
+  end
+
+  # Get a global application command by ID.
+  # https://discord.com/developers/docs/interactions/slash-commands#get-global-application-command
+  def get_global_command(token, application_id, command_id)
+    Discordrb::API.request(
+      :applications_aid_commands_cid,
+      nil,
+      :get,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/commands/#{command_id}",
       Authorization: token
     )
   end
@@ -32,7 +44,7 @@ module Discordrb::API::Application
 
   # Edit a global application command.
   # https://discord.com/developers/docs/interactions/slash-commands#edit-global-application-command
-  def edit_global_command(token, application_id, command_id, name: nil, description: nil, options: nil)
+  def edit_global_command(token, application_id, command_id, name = nil, description = nil, options = nil)
     Discordrb::API.request(
       :applications_aid_commands_cid,
       nil,
@@ -56,6 +68,20 @@ module Discordrb::API::Application
     )
   end
 
+  # Set global application commands in bulk.
+  # https://discord.com/developers/docs/interactions/slash-commands#bulk-overwrite-global-application-commands
+  def bulk_overwrite_global_commands(token, application_id, commands)
+    Discordrb::API.request(
+      :applications_aid_commands,
+      nil,
+      :put,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/commands",
+      commands.to_json,
+      Authorization: token,
+      content_type: :json
+    )
+  end
+
   # Get a guild's commands for an application.
   # https://discord.com/developers/docs/interactions/slash-commands#get-guild-application-commands
   def get_guild_commands(token, application_id, guild_id)
@@ -64,6 +90,18 @@ module Discordrb::API::Application
       guild_id,
       :get,
       "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands",
+      Authorization: token
+    )
+  end
+
+  # Get a guild command by ID.
+  # https://discord.com/developers/docs/interactions/slash-commands#get-guild-application-command
+  def get_guild_command(token, application_id, guild_id, command_id)
+    Discordrb::API.request(
+      :applications_aid_guilds_gid_commands_cid,
+      guild_id,
+      :get,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands/#{command_id}",
       Authorization: token
     )
   end
@@ -84,7 +122,7 @@ module Discordrb::API::Application
 
   # Edit an application command for a guild.
   # https://discord.com/developers/docs/interactions/slash-commands#edit-guild-application-command
-  def edit_guild_command(token, application_id, guild_id, command_id, name: nil, description: nil, options: nil)
+  def edit_guild_command(token, application_id, guild_id, command_id, name = nil, description = nil, options = nil)
     Discordrb::API.request(
       :applications_aid_guilds_gid_commands_cid,
       guild_id,
@@ -105,6 +143,60 @@ module Discordrb::API::Application
       :delete,
       "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands/#{command_id}",
       Authorization: token
+    )
+  end
+
+  # Set guild commands in bulk.
+  # https://discord.com/developers/docs/interactions/slash-commands#bulk-overwrite-guild-application-commands
+  def bulk_overwrite_guild_commands(token, application_id, guild_id, commands)
+    Discordrb::API.request(
+      :applications_aid_guilds_gid_commands,
+      guild_id,
+      :put,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands",
+      commands.to_json,
+      Authorization: token,
+      content_type: :json
+    )
+  end
+
+  # Get the permissions for a specific guild command.
+  # https://discord.com/developers/docs/interactions/slash-commands#get-application-command-permissions
+  def get_guild_command_permissions(token, application_id, guild_id)
+    Discordrb::API.request(
+      :applications_aid_guilds_gid_commands_permissions,
+      guild_id,
+      :get,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands/permissions",
+      Authorization: token
+    )
+  end
+
+  # Edit the permissions for a specific guild command.
+  # https://discord.com/developers/docs/interactions/slash-commands#edit-application-command-permissions
+  def edit_guild_command_permissions(token, application_id, guild_id, command_id, permissions)
+    Discordrb::API.request(
+      :applications_aid_guilds_gid_commands_cid_permissions,
+      guild_id,
+      :put,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands/#{command_id}/permissions",
+      { permissions: permissions }.to_json,
+      Authorization: token,
+      content_type: :json
+    )
+  end
+
+  # Edit permissions for all commands in a guild.
+  # https://discord.com/developers/docs/interactions/slash-commands#batch-edit-application-command-permissions
+  def batch_edit_command_permissions(token, application_id, guild_id, permissions)
+    Discordrb::API.request(
+      :applications_aid_guilds_gid_commands_cid_permissions,
+      guild_id,
+      :put,
+      "#{Discordrb::API.api_base}/applications/#{application_id}/guilds/#{guild_id}/commands/permissions",
+      permissions.to_json,
+      Authorization: token,
+      content_type: :json
     )
   end
 end
