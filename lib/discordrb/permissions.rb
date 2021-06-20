508f7379c22dda44bc4b1e097b3d8cf26d0cd1e3
Matthew Carey <matthew.b.carey@gmail.com>
2021-06-20 13:13:37 -0400

    fix(PermissionCalculator): process roles via the role method in permission calculator

--- lib/discordrb/permissions.rb
+++ lib/discordrb/permissions.rb
@@ -179,7 +179,7 @@ module Discordrb
     private
 
     def defined_role_permission?(action, channel)
-      roles_to_check = [@server.everyone_role] + @roles
+      roles_to_check = [@server.everyone_role] + roles
 
       # For each role, check if
       #   (1) the channel explicitly allows or permits an action for the role and
