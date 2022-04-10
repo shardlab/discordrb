6e0734e20a4c53b0a5ba19077e89e164dbe1d228
Adrien Smith <adrien@panissupraomnia.dev>
2022-04-10 16:49:34 -0600

    Add pre-implementation spec for helper method

---
+++ spec/api_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'discordrb'
+
+describe Discordrb::API do
+  describe '.icon_size', pending: 'implementing' do
+    it 'returns nil if argument is nil' do
+      expect(Discordrb::API.icon_size(nil)).to be_nil
+    end
+
+    it 'returns a string if argument is an int' do
+      expect(Discordrb::API.icon_size(16)).to be_a(String)
+    end
+
+    it 'returns a string in the correct format' do
+      expect(Discordrb::API.icon_size(16)).to eq('?size=16')
+    end
+  end
+end
