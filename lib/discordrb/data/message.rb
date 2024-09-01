d9bdd71afe3eb1a700d3460a318cb765ac9b236f
Droid000 <johnship2876@gmail.com>
2024-08-31 20:49:59 -0400

    Update method name.

--- lib/discordrb/data/message.rb
+++ lib/discordrb/data/message.rb
@@ -283,7 +283,7 @@ module Discordrb
 
     # Check if a sticker was used in this message.
     # @return [true, false] whether or not a sticker was used
-    def sticker?
+    def any_sticker?
       !(sticker || '').empty?
     end
 
