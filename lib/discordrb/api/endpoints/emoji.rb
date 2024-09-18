687873aa555c3243f9caa4e0139d1b1b4dded533
Droid000 <johnship2876@gmail.com>
2024-09-18 17:01:57 -0400

    Feat: V4.0 Application emoji endpoints.

--- lib/discordrb/api/endpoints/emoji.rb
+++ lib/discordrb/api/endpoints/emoji.rb
@@ -53,6 +53,51 @@ module Discordrb
         request Route[:DELETE, "/guilds/#{guild_id}/emojis/#{emoji_id}"],
                 reason: reason
       end
+
+      # @!discord_api https://discord.com/developers/docs/resources/emoji#list-application-emojis
+      # @return [Array<Hash<Symbol, Object>>]
+      def list_application_emojis(application_id, **params)
+        request Route[:GET, "/applications/#{application_id}/emojis", application_id],
+                params: params
+      end
+
+      # @!discord_api https://discord.com/developers/docs/resources/emoji#get-application-emoji
+      # @return [Hash<Symbol, Object>]
+      def get_application_emoji(application_id, emoji_id, **params)
+        request Route[:GET, "/applications/#{application_id}/emojis/#{emoji_id}", application_id],
+                params: params
+      end
+
+      # @!discord_api https://discord.com/developers/docs/resources/emoji#create-application-emoji
+      # @return [Hash<Symbol, Object>]
+      def create_application_emoji(application_id, name:, image:, **rest)
+        data = {
+          name: name,
+          image: image,
+          **rest
+        }
+
+        request Route[:POST, "/applications/#{application_id}/emojis"],
+                body: filter_undef(data)
+      end
+
+      # @!discord_api https://discord.com/developers/docs/resources/emoji#modify-application-emoji
+      # @return [Hash<Symbol, Object>]
+      def modify_application_emoji(application_id, emoji_id, name: :undef, **rest)
+        data = {
+          name: name,
+          **rest
+        }
+
+        request Route[:PATCH, "/applications/#{application_id}/emojis/#{emoji_id}"],
+                body: filter_undef(data)
+      end
+
+      # @!discord_api https://discord.com/developers/docs/resources/emoji#delete-application-emoji
+      # @return [nil]
+      def delete_application_emoji(application_id, emoji_id)
+        request Route[:DELETE, "/applications/#{application_id}/emojis/#{emoji_id}"],
+      end
     end
   end
 end
