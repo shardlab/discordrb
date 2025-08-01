a49bb8ee256cb8bfb7e2a4918664f1264e5d408a
Droid <johnship2876@gmail.com>
2025-08-01 03:53:13 -0400

    feat: server previews

--- lib/discordrb/api/server.rb
+++ lib/discordrb/api/server.rb
@@ -4,20 +4,6 @@
 module Discordrb::API::Server
   module_function
 
-  # Create a server
-  # https://discord.com/developers/docs/resources/guild#create-guild
-  def create(token, name, region = :'eu-central')
-    Discordrb::API.request(
-      :guilds,
-      nil,
-      :post,
-      "#{Discordrb::API.api_base}/guilds",
-      { name: name, region: region.to_s }.to_json,
-      Authorization: token,
-      content_type: :json
-    )
-  end
-
   # Get a server's data
   # https://discord.com/developers/docs/resources/guild#get-guild
   def resolve(token, server_id, with_counts = nil)
@@ -45,33 +31,6 @@ module Discordrb::API::Server
     )
   end
 
-  # Transfer server ownership
-  # https://discord.com/developers/docs/resources/guild#modify-guild
-  def transfer_ownership(token, server_id, user_id, reason = nil)
-    Discordrb::API.request(
-      :guilds_sid,
-      server_id,
-      :patch,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}",
-      { owner_id: user_id }.to_json,
-      Authorization: token,
-      content_type: :json,
-      'X-Audit-Log-Reason': reason
-    )
-  end
-
-  # Delete a server
-  # https://discord.com/developers/docs/resources/guild#delete-guild
-  def delete(token, server_id)
-    Discordrb::API.request(
-      :guilds_sid,
-      server_id,
-      :delete,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}",
-      Authorization: token
-    )
-  end
-
   # Get a server's channels list
   # https://discord.com/developers/docs/resources/guild#get-guild-channels
   def channels(token, server_id)
@@ -96,6 +55,18 @@ module Discordrb::API::Server
       Authorization: token,
       content_type: :json,
       'X-Audit-Log-Reason': reason
+    )
+  end
+
+  # Get the preview of a server.
+  # https://discord.com/developers/docs/resources/guild#get-guild-preview
+  def preview(token, server_id)
+    Discordrb::API.request(
+      :guilds_sid_preview,
+      server_id,
+      :get,
+      "#{Discordrb::API.api_base}/guilds/#{server_id}/preview",
+      Authorization: token
     )
   end
 
