d91420f5af74d3f988e7a156c5ac4fe020d92dd7
Droid <174426320+Droid00000@users.noreply.github.com>
2026-04-12 19:41:26 -0400

    feat: polls

--- lib/discordrb.rb
+++ lib/discordrb.rb
@@ -33,7 +33,9 @@ module Discordrb
     direct_messages: 1 << 12,
     direct_message_reactions: 1 << 13,
     direct_message_typing: 1 << 14,
-    server_scheduled_events: 1 << 16
+    server_scheduled_events: 1 << 16,
+    server_message_polls: 1 << 24,
+    direct_message_polls: 1 << 25
   }.freeze
 
   # All available intents
