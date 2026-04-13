8f38a23f727d7d662d770417c2335aa58406ef2e
Droid <174426320+Droid00000@users.noreply.github.com>
2026-04-12 20:43:53 -0400

    feat: Message#modify

--- lib/discordrb/data/message.rb
+++ lib/discordrb/data/message.rb
@@ -402,12 +402,10 @@ module Discordrb
       @reactions.select(&:me)
     end
 
-    # Removes embeds from the message
+    # Removes embeds from the message.
     # @return [Message] the resulting message.
     def suppress_embeds
-      flags = @flags | (1 << 2)
-      response = API::Channel.edit_message(@bot.token, @channel.id, @id, :undef, :undef, :undef, :undef, flags)
-      Message.new(JSON.parse(response), @bot)
+      modify(flags: @flags | FLAGS[:suppress_embeds])
     end
 
     # Check if this message mentions a specific user or role.
@@ -502,7 +500,7 @@ module Discordrb
 
     # The inspect method is overwritten to give more useful output
     def inspect
-      "<Message content=\"#{@content}\" id=#{@id} timestamp=#{@timestamp} author=#{@author} channel=#{@channel}>"
+      "<Message id=#{@id} channel_id=#{@channel.id} content=\"#{@content}\" creation_time=#{creation_time}>"
     end
 
     # @return [String] a URL that a user can use to navigate to this message in the client
@@ -602,6 +600,75 @@ module Discordrb
       end
 
       @timestamps
+    end
+
+    # Edit the properties of the message.
+    # @param content [String] The new content of the message. Should not be longer than 2000 characters.
+    # @param embeds [Array<Hash, Webhooks::Embed>] The new embeds that should be attached to the message.
+    # @param flags [Integer, Symbol, Array<Symbol, Integer>] The new message flags that should be applied.
+    # @param components [View, Array<#to_h>] The new message components that should be set for the message.
+    # @param has_components [true, false] Whether to set the `:uikit_components` flag when updating the message.
+    # @param attachments [Array<File, Hash, Integer, String, Attachment>] The new attachments to set for the message.
+    # @param retain_attachments [true, false] Whether or not to retain the exisiting attachments present on the message.
+    # @yieldparam builder [Webhooks::Builder] An optional message builder. Overwrites arguments passed to the method.
+    # @yieldparam view [Webhooks::View] An optional component builder. Overwrites arguments passed to the method.
+    # @note Any arguments that you pass will completely overwrite the old value. For example, passing `flags` will completely
+    #   replace the `flags` field, passing `embeds` will completely replace all the `embeds`, etc.
+    # @return [Message] the updated message.
+    def modify(
+      content: :undef, embeds: :undef, flags: :undef, components: :undef, attachments: :undef,
+      retain_attachments: false, has_components: :undef
+    )
+      builder = Discordrb::Webhooks::Builder.new
+      view = Discordrb::Webhooks::View.new
+
+      builder.content = content
+      embeds&.each { |embed| builder << embed } unless embeds == :undef
+      yield(builder, view) if block_given?
+
+      flags = Array(flags).map { |flag| FLAGS[flag] || flag }.reduce(0, &:|) if flags != :undef
+      components = components&.to_a unless components == :undef
+      builder = builder.to_json_hash
+
+      if has_components == true
+        flags == :undef ? (flags = (@flags | FLAGS[:uikit_components])) : flags |= FLAGS[:uikit_components]
+      end
+
+      builder = {
+        flags: flags,
+        content: builder[:content],
+        components: block_given? && view.to_a.any? ? view.to_a : components,
+        embeds: block_given? && builder[:embeds]&.any? ? builder[:embeds] : embeds
+      }
+
+      if flags != :undef && !uikit_components? && flags.anybits?(FLAGS[:uikit_components])
+        builder[:poll] = nil
+        builder[:embeds] = []
+        builder[:content] = nil
+      end
+
+      if attachments.is_a?(Array) && attachments.any?
+        attachments.each do |attachment|
+          case attachment
+          when String, Integer, Attachment
+            (builder[:attachments] ||= []) << { id: attachment.resolve_id }
+          when Hash
+            (builder[:attachments] ||= []) << attachment
+          else
+            (builder[:files] ||= []) << attachment
+          end
+        end
+      elsif attachments != :undef
+        builder[:attachments] = attachments
+      end
+
+      # rubocop:disable Style/IfUnlessModifier
+      if builder[:files] && retain_attachments == true && @attachments.any?
+        (builder[:attachments] ||= []).push(*@attachments.map { |attachment| { id: attachment.id } })
+      end
+
+      # rubocop:enable Style/IfUnlessModifier
+      Message.new(JSON.parse(API::Channel.update_message(@bot.token, @channel.id, @id, **builder)), @bot)
     end
   end
 end
